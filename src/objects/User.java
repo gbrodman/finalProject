e47352729cf0cd0491b7176ab0e863420395fb01
zsaraf <zsaraf@stanford.edu>
2013-03-03 17:56:31 -0500

    Added friends and updated user to user picture

--- src/objects/User.java
+++ src/objects/User.java
@@ -5,15 +5,19 @@ import java.sql.SQLException;
 import java.util.List;
 
 import database.AchievementUtils;
+import database.FriendUtils;
 import database.QuizUtils;
 
 public class User {
 
 	private String name;
 	private List<Message> inbox;
-	private List<User> friends;
+	private List<String> friends;
+	private List<String> friendRequestsSent;
+	private List<String> friendRequestsRecieved;
 	private List<Quiz> ownedQuizzes;
 	private List<Achievement> achievements;
+	private String photoURL;
 	private boolean isAdmin;
 	
 	// 0 is no privacy, 1 is name-only, 2 is anonymous
@@ -29,6 +33,10 @@ public class User {
 				name = rs.getString("username");
 				privacySetting = rs.getInt("privacySetting");
 				isAdmin = rs.getInt("isAdmin") == 1;
+				photoURL = rs.getString("photoURL");
+				friends = FriendUtils.getFriends(name);
+				friendRequestsSent = FriendUtils.getSentRequests(name);
+				friendRequestsRecieved = FriendUtils.getRecievedRequests(name);
 				String achievementIDs = rs.getString("achievementIDs");
 				achievements = AchievementUtils.getAchievements(achievementIDs);
 				ownedQuizzes = QuizUtils.getQuizzesByUser(name);
@@ -41,13 +49,25 @@ public class User {
 	public String getName() {
 		return name;
 	}
+	
+	public String getPhotoURL() {
+		return photoURL;
+	}
 
 	public List<Message> getInbox() {
 		return inbox;
 	}
 
-	public List<User> getFriends() {
+	public List<String> getFriends() {
 		return friends;
+	}
+	
+	public List<String> getFriendRequestsSent() {
+		return friendRequestsSent;
+	}
+	
+	public List<String> getFriendRequestsRecieved() {
+		return friendRequestsRecieved;
 	}
 
 	public List<Quiz> getOwnedQuizzes() {
