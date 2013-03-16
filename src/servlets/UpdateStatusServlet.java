4b82ea332b4091b0717a5de5dcdda68fa9b92b6d
Gustav Brodman <gus.brodman@gmail.com>
2013-03-15 18:24:55 -0700

    aoiuhsdfouashdf

---
+++ src/servlets/UpdateStatusServlet.java
@@ -0,0 +1,49 @@
+package servlets;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import objects.User;
+import database.UserUtils;
+
+/**
+ * Servlet implementation class UpdateStatusServlet
+ */
+@WebServlet("/UpdateStatusServlet")
+public class UpdateStatusServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public UpdateStatusServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		User user = (User) request.getSession().getAttribute("user");
+		String status = request.getParameter("status");
+		user.setStatus(status);
+		UserUtils.setStatus(user);
+		RequestDispatcher dispatcher = request.getRequestDispatcher("Homepage.jsp");
+		dispatcher.forward(request, response);
+	}
+
+}
